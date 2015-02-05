32cc775b35b5a5a10cb3ebb07c3cfbb775259bca
Jaap Jansma <jaap@edeveloper.nl>
2015-02-05 15:53:33 +0100

    added sending of multiple contacts

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ SMS API for CiviCRM to send sms through the API
 
 The entity for the SMS API is SMS and the action is Send.
 Parameters for the api are specified below:
-- contact_id: contact which will receive the e-mail
+- contact_id: list of contacts IDs to create the PDF Letter (separated by ",")
 - template_id: ID of the message template which will be used in the API. 
 - provider_id: ID of the SMS provider
 - from_contact_id: (**Not required**) the contact ID who is sending the sms
--- api/v3/Sms/Send.php
+++ api/v3/Sms/Send.php
@@ -24,50 +24,60 @@ function _civicrm_api3_sms_send_spec(&$spec) {
  * @throws API_Exception
  */
 function civicrm_api3_sms_send($params) {
-  $messageTemplates = new CRM_Core_DAO_MessageTemplates();
+  $version = CRM_Core_BAO_Domain::version();
+  if (!preg_match('/[0-9]+(,[0-9]+)*/i', $params['contact_id'])) {
+    throw new API_Exception('Parameter contact_id must be a unique id or a list of ids separated by comma');
+  }
+  $contactIds = explode(",", $params['contact_id']);
+
+  // Compatibility with CiviCRM > 4.3
+  if($version >= 4.4) {
+    $messageTemplates = new CRM_Core_DAO_MessageTemplate();
+  } else {
+    $messageTemplates = new CRM_Core_DAO_MessageTemplates();
+  }
   $messageTemplates->id = $params['template_id'];
   
   if (!$messageTemplates->find(TRUE)) {
     throw new API_Exception('Could not find template with ID: '.$params['template_id']);
   }
   
-  $contactId = $params['contact_id'];
+  foreach($contactIds as $contactId) {
+    $returnProperties = array(
+        'sort_name' => 1,
+        'phone' => 1,
+        'do_not_sms' => 1,
+        'is_deceased' => 1,
+        'display_name' => 1,
+    );
+    list($contactDetails) = CRM_Utils_Token::getTokenDetails(array($contactId), $returnProperties, FALSE, FALSE);
 
-  $returnProperties = array(
-      'sort_name' => 1,
-      'phone' => 1,
-      'do_not_sms' => 1,
-      'is_deceased' => 1,
-      'display_name' => 1,
-  );
-  list($contactDetails) = CRM_Utils_Token::getTokenDetails(array($contactId), $returnProperties, FALSE, FALSE);
+    //to check if the phone type is "Mobile"
+    $phoneTypes = CRM_Core_OptionGroup::values('phone_type', TRUE, FALSE, FALSE, NULL, 'name');
+    $phone = CRM_Core_DAO::executeQuery('SELECT * FROM civicrm_phone WHERE phone_type_id = %1 AND contact_id = %2', array(
+        1 => array(CRM_Utils_Array::value('Mobile', $phoneTypes), 'Integer'),
+        2 => array($contactId, 'Integer'),
+    ));
+    if (!$phone->fetch()) {
+      throw new API_Exception('Suppressed sending sms to: ' . $contactDetails[$contactId]['display_name']);
+    }
+    $contactDetails[$contactId]['phone_id'] = $phone->id;
+    $contactDetails[$contactId]['phone'] = $phone->phone;
+    $contactDetails[$contactId]['phone_type_id'] = CRM_Utils_Array::value('Mobile', $phoneTypes);
 
-  //to check if the phone type is "Mobile"
-  $phoneTypes = CRM_Core_OptionGroup::values('phone_type', TRUE, FALSE, FALSE, NULL, 'name');
-  $phone = CRM_Core_DAO::executeQuery('SELECT * FROM civicrm_phone WHERE phone_type_id = %1 AND contact_id = %2', array(
-      1 => array(CRM_Utils_Array::value('Mobile', $phoneTypes), 'Integer'),
-      2 => array($contactId, 'Integer'),
-  ));
-  if (!$phone->fetch()) {
-    throw new API_Exception('Suppressed sending sms to: '.$contactDetails[$contactId]['display_name']);
+    $activityParams['html_message'] = $messageTemplates->html_text;
+    $activityParams['text_message'] = $messageTemplates->msg_text;
+    $activityParams['activity_subject'] = $messageTemplates->msg_subject;
+    $smsParams['provider_id'] = $params['provider_id'];
+
+    $from_contact_id = null;
+    if (isset($params['from_contact_id'])) {
+      $from_contact_id = $params['from_contact_id'];
+    }
+
+    $contactIds = array($contactId);
+    $return = CRM_Activity_BAO_Activity::sendSMS($contactDetails, $activityParams, $smsParams, $contactIds, $from_contact_id);
   }
-  $contactDetails[$contactId]['phone_id'] = $phone->id;
-  $contactDetails[$contactId]['phone'] = $phone->phone;
-  $contactDetails[$contactId]['phone_type_id'] = CRM_Utils_Array::value('Mobile', $phoneTypes);
-
-  $activityParams['html_message'] = $messageTemplates->html_text;
-  $activityParams['text_message'] = $messageTemplates->msg_text;
-  $activityParams['activity_subject'] = $messageTemplates->msg_subject;
-  $smsParams['provider_id'] = $params['provider_id'];
-  
-  $from_contact_id = null;
-  if (isset($params['from_contact_id'])) {
-    $from_contact_id = $params['from_contact_id'];
-  }
-
-  $contactIds = array($contactId);
-  $return = CRM_Activity_BAO_Activity::sendSMS($contactDetails, $activityParams, $smsParams, $contactIds, $from_contact_id);
-  
   $returnValues = array();
   return civicrm_api3_create_success($returnValues, $params, 'Sms', 'Send');
 }
