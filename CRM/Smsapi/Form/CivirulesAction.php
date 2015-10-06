a6d20bc8b6fb53c3b73a16b7734001a42e22eae1
Jaap Jansma <jaap@edeveloper.nl>
2015-10-06 17:06:42 +0200

    civirules edit of action did not show saved values

--- CRM/Smsapi/Form/CivirulesAction.php
+++ CRM/Smsapi/Form/CivirulesAction.php
@@ -101,7 +101,7 @@ class CRM_Smsapi_Form_CivirulesAction extends CRM_Core_Form {
     $defaultValues = array();
     $defaultValues['rule_action_id'] = $this->ruleActionId;
     if (!empty($this->ruleAction->action_params)) {
-      $data = unserialize($this->ruleCondition->action_params);
+      $data = unserialize($this->ruleAction->action_params);
     }
     if (!empty($data['provider_id'])) {
       $defaultValues['provider_id'] = $data['provider_id'];
